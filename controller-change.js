ac535faa8188f201565c878bcf87c8ff1cf1e518
Angelo C <angmogollon@gmail.com>
2023-05-19 17:27:58 -0300

    Adding switch camera button

--- controller-change.js
+++ controller-change.js
@@ -1,5 +1,6 @@
 const imageBtnEle = document.getElementById("image-btn");
 const planeElement = document.getElementById("medical-image");
+const switchCameraButton = document.getElementById("camera-btn");
 const imagesArray = [
   "./examples/img/ray1.jpg?raw=true",
   "./examples/img/ray2.jpg?raw=true",
@@ -8,6 +9,10 @@ const imagesArray = [
 
 function onConnect() {
   planeElement.setAttribute('src', imagesArray[0]);
+  switchCameraButton.addEventListener('click', () => {
+	  arSystem.switchCamera();
+	});
+
 imageBtnEle.addEventListener("click", function () {
   let index = imagesArray.indexOf(planeElement.getAttribute("src"));
   planeElement.setAttribute('src', imagesArray[index + 1]);
